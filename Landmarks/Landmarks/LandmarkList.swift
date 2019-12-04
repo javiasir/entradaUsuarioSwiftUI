374947ec09e3e9dbc9fd17b66f61a23fba24fece
Javi <33642025+javiasir@users.noreply.github.com>
2019-12-04 02:44:17 +0100

    Aplicacion terminada
    Añadido el boton favoritos en la vista de los elementos.

--- Landmarks/Landmarks/LandmarkList.swift
+++ Landmarks/Landmarks/LandmarkList.swift
@@ -12,17 +12,28 @@ struct LandmarkList: View {
     //@State var showFavoritesOnly = false
     
     //Lo modificamos a true para el añadido del boton del filtrado de si queremos que aparezcan los favoritos o no. Y abajo añadimos el bucle ForEach, para combinar diferentes vistas dinámicas.
-    @State var showFavoritesOnly = true
+    // @State var showFavoritesOnly = true
+    
+    //Una vez creado el observable, que hace de almacenamiento para las propiedades con los valores iniciales que le indiquemos.
+    @EnvironmentObject var userData: UserData
+    var landmark: Landmark
+    
+    var landmarkIndex: Int {
+        userData.landmarks.firstIndex(where: {$0.id == landmark.id})!
+    }
     
     var body: some View {
         NavigationView {
             List {
                 // Con el toggle indicamos que cree la barra con el botón, con el mensaje para mostrar solo los favoritos.
-                Toggle(isOn: $showFavoritesOnly) {
+                //Toggle(isOn: $showFavoritesOnly) {
+                
+                // Añadimos los usos de userdata.
+                Toggle (isOn: $userData.showFavoritesOnly) {
                     Text("Favorites Only")
                 }
-            ForEach (landmarkData) { landmark in
-                if !self.showFavoritesOnly || landmark.isFavorite {
+                ForEach (userData.landmarkData) { landmark in
+                if !self.userData.showFavoritesOnly || landmark.isFavorite {
                 NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
                     LandmarkRow(landmark: landmark)
                         }
@@ -37,9 +48,10 @@ struct LandmarkList: View {
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
         ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
-            LandmarkList()
+            LandmarkList(landmark: <#Landmark#>)
                 .previewDevice(PreviewDevice(rawValue: deviceName))
                 .previewDisplayName(deviceName)
         }
+        .environmentObject(UserData())
     }
 }
