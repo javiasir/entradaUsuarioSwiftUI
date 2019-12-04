54fc181bc13f1e26ce35b8bb8fdddebe66c692a4
Javi <33642025+javiasir@users.noreply.github.com>
2019-12-04 01:57:18 +0100

    Boton de favoritos
    Añadido en LandmarkList el toggle, que añade el botón para mostrar los favoritos.

--- Landmarks/Landmarks/LandmarkList.swift
+++ Landmarks/Landmarks/LandmarkList.swift
@@ -9,14 +9,23 @@ import SwiftUI
 
 struct LandmarkList: View {
     // @State es un valor o conjunto de valores que pueden cambiar con el tiempo y afecta al comportamiento de la vista, contenido o diseño.
-    @State var showFavoritesOnly = false
+    //@State var showFavoritesOnly = false
+    
+    //Lo modificamos a true para el añadido del boton del filtrado de si queremos que aparezcan los favoritos o no. Y abajo añadimos el bucle ForEach, para combinar diferentes vistas dinámicas.
+    @State var showFavoritesOnly = true
     
     var body: some View {
         NavigationView {
-            List(landmarkData) { landmark in
+            List {
+                // Con el toggle indicamos que cree la barra con el botón, con el mensaje para mostrar solo los favoritos.
+                Toggle(isOn: $showFavoritesOnly) {
+                    Text("Favorites Only")
+                }
+            ForEach (landmarkData) { landmark in
                 if !self.showFavoritesOnly || landmark.isFavorite {
                 NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
                     LandmarkRow(landmark: landmark)
+                        }
                     }
                 }
             }
